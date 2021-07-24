2853aef4ff7c3abe95d5e79f35d10b123d4db8da
AlvaroIsrael <israel.nunes.leite@gmail.com>
2021-07-23 23:39:44 -0300

    feat(👑): Created a new interface to handle authentication token creation.

---
+++ src/interfaces/IPersonUpdateRequest.ts
@@ -0,0 +1,16 @@
+export type IPersonUpdateRequest = {
+  personId: string;
+  kind: 'legal' | 'natural';
+  role: 'admin' | 'default';
+  corporateName: string;
+  document: string;
+  documentName?: 'cpf' | 'cnpj';
+  name: string;
+  email: string;
+  password: string;
+  landlinePhoneNumber: string;
+  mobilePhoneNumber: string;
+  avatarUrl: string;
+  sex: 'masculine' | 'feminine';
+  birthDate: Date;
+};
