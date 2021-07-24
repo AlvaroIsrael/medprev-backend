0558e5b7edc0582ce0eac62e64af950fae053560
AlvaroIsrael <israel.nunes.leite@gmail.com>
2021-07-23 22:06:47 -0300

    feat(👑): Improvement of server.ts to take into consideration port number from .env file.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import app from './app';
 
-const port = process.env.PORT || 3333;
+const getPort = (url: string | undefined): string => {
+  const port = url === undefined ? [] : url.split(':');
+  const portExist = port.length > 0;
+  return portExist ? port[port.length] : '3333';
+};
+
+const port = getPort(process.env.APP_API_URL);
 
 app.listen(port, () => {
-  console.log('🔥 Server running on port 3333! 🔥');
+  console.log(`🔥 Server running on port ${port} ! 🔥`);
 });
